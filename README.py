6a7badfe2088820fd155d60b1806b7f0d31dca71
gigila207-rgb <gigila207@gmail.com>
2026-07-11 19:13:24 +0100

    Update and rename README.md to README.py

---
+++ README.py
@@ -0,0 +1,16 @@
+# python-projects
+num = float(input("enter first number :"))
+num2 = float(input("enter second number :"))
+operation = input("choose operation :")
+if operation = +:
+  print(num + num2)
+elif operation = -:
+   print(num - num2)
+elif  operation = *:
+  print(num * num2)
+elif  operation = /:
+  print(num / num2)
+
+
+
+  
