de6d625aeeeab6cabc92735bec6442785c24ced1
gigila207-rgb <gigila207@gmail.com>
2026-07-12 20:22:07 +0100

    Add grade evaluation logic in check.py

---
+++ check.py
@@ -0,0 +1,10 @@
+grade = int(input("enter your grade :"))
+if grade >=‎ 16 :
+  print("excellent")
+elif  grade ‎>=‎ 14 :
+  print("very good ")
+elif  grade ‎>=‎ 10 :
+  print("passed ")
+else :
+  print("failed")
+
