902ee3d975e39f8ff3da12c5644929fa8de05481
gigila207-rgb <gigila207@gmail.com>
2026-08-02 20:43:25 +0100

    Initialize grade management system with options

---
+++ grade.py
@@ -0,0 +1,16 @@
+grades = {
+    "Ali": 17,
+    "Sara": 19,
+    "Adam": 15
+}
+print("""1. Add Student
+2. Search Student
+3. Update Grade
+4. Show All Grades
+5. Best Student
+6. Exit """)
+while True:
+  choose =int(input("select an operation :"))
+  if choose == 1:
+    grades.add
+    
