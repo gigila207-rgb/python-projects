9754da5cd2cf68d4549be32345048d328cfc18be
gigila207-rgb <gigila207@gmail.com>
2026-08-02 20:53:08 +0100

    Implement student grade management features

--- grade.py
+++ grade.py
@@ -11,6 +11,31 @@ print("""1. Add Student
 6. Exit """)
 while True:
   choose =int(input("select an operation :"))
-  if choose == 1:
-    grades.add
+    if choose == 1:
+        student = input("add the name of the new student : ")
+        grade = input("add the the grade : ")
+        grades[student] = grade 
+
+    elif choose == 2:
+        search_student = input("search for a student: ")
+        if search_student in grades:
+            print(grades.get( search_student ))
+        else:
+            print("Contact not found.")
+    elif choose == 3:
+        search_student = input("search for a student: ")
+        new_grade = input("add the updated grade  : ")
+        grades[search_student] = new_grade 
+        
+     elif choose == 4:
+        
+    
+    
+        
+        
+        
+        
+    
+
+  
     
